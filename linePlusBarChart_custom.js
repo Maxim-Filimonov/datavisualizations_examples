195a6b0db5b8a149e9aaeca063ba43ed4248a271
Maxim Filimonov <tpaktopsp@gmail.com>
2013-06-30 22:52:15 +1000

    Add bar chart without line yet

---
+++ linePlusBarChart_custom.js
@@ -0,0 +1,350 @@
+var data = [
+[
+"2013-06-28T21:58:12+10:00",
+  [
+{
+  "order_item": {
+    "amount": "450.0",
+    "created_at": "2013-06-28T11:58:12Z",
+    "id": 249,
+    "menu_item_id": 10,
+    "name": "Ciroc",
+    "order_for": "2013-06-28T11:58:12Z",
+    "order_id": 135,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:03:31Z"
+  }
+},
+{
+  "order_item": {
+    "amount": "495.0",
+    "created_at": "2013-06-28T11:58:12Z",
+    "id": 248,
+    "menu_item_id": 17,
+    "name": "Dom Pérignon (2003)",
+    "order_for": "2013-06-28T11:58:12Z",
+    "order_id": 135,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:03:31Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:06:27+10:00",
+  [
+{
+  "order_item": {
+    "amount": "1450.0",
+    "created_at": "2013-06-28T12:06:27Z",
+    "id": 251,
+    "menu_item_id": 36,
+    "name": "Dom Pérignon Rosé (1998)",
+    "order_for": "2013-06-28T12:06:27Z",
+    "order_id": 136,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:09:33Z"
+  }
+},
+{
+  "order_item": {
+    "amount": "1450.0",
+    "created_at": "2013-06-28T12:06:27Z",
+    "id": 250,
+    "menu_item_id": 36,
+    "name": "Dom Pérignon Rosé (1998)",
+    "order_for": "2013-06-28T12:06:27Z",
+    "order_id": 136,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:09:33Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:09:49+10:00",
+  [
+{
+  "order_item": {
+    "amount": "385.0",
+    "created_at": "2013-06-28T12:09:49Z",
+    "id": 252,
+    "menu_item_id": 60,
+    "name": "Jack Daniels",
+    "order_for": "2013-06-28T12:09:49Z",
+    "order_id": 137,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:16:29Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:10:20+10:00",
+  [
+{
+  "order_item": {
+    "amount": "400.0",
+    "created_at": "2013-06-28T12:10:20Z",
+    "id": 253,
+    "menu_item_id": 3,
+    "name": "Belvedere",
+    "order_for": "2013-06-28T12:10:20Z",
+    "order_id": 138,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:16:30Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:22:47+10:00",
+  [
+{
+  "order_item": {
+    "amount": "975.0",
+    "created_at": "2013-06-28T12:22:47Z",
+    "id": 254,
+    "menu_item_id": 68,
+    "name": "Belvedere Magnum (1.75L)",
+    "order_for": "2013-06-28T12:22:47Z",
+    "order_id": 139,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:26:05Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:29:06+10:00",
+  [
+{
+  "order_item": {
+    "amount": "975.0",
+    "created_at": "2013-06-28T12:29:06Z",
+    "id": 255,
+    "menu_item_id": 68,
+    "name": "Belvedere Magnum (1.75L)",
+    "order_for": "2013-06-28T12:29:06Z",
+    "order_id": 140,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:31:50Z"
+  }
+},
+{
+  "order_item": {
+    "amount": "350.0",
+    "created_at": "2013-06-28T12:29:06Z",
+    "id": 256,
+    "menu_item_id": 33,
+    "name": "Wet Pussy Premix",
+    "order_for": "2013-06-28T12:29:06Z",
+    "order_id": 140,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:31:52Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:36:08+10:00",
+  [
+{
+  "order_item": {
+    "amount": "400.0",
+    "created_at": "2013-06-28T12:36:08Z",
+    "id": 257,
+    "menu_item_id": 3,
+    "name": "Belvedere",
+    "order_for": "2013-06-28T12:36:08Z",
+    "order_id": 141,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:36:20Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T22:49:36+10:00",
+  [
+{
+  "order_item": {
+    "amount": "450.0",
+    "created_at": "2013-06-28T12:49:36Z",
+    "id": 258,
+    "menu_item_id": 57,
+    "name": "Chivas 12 Year",
+    "order_for": "2013-06-28T12:49:36Z",
+    "order_id": 142,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:50:51Z"
+  }
+},
+{
+  "order_item": {
+    "amount": "975.0",
+    "created_at": "2013-06-28T12:49:36Z",
+    "id": 259,
+    "menu_item_id": 68,
+    "name": "Belvedere Magnum (1.75L)",
+    "order_for": "2013-06-28T12:49:36Z",
+    "order_id": 142,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T12:50:53Z"
+  }
+}
+]
+],
+  [
+  "2013-06-28T23:03:43+10:00",
+  [
+{
+  "order_item": {
+    "amount": "400.0",
+    "created_at": "2013-06-28T13:03:43Z",
+    "id": 260,
+    "menu_item_id": 3,
+    "name": "Belvedere",
+    "order_for": "2013-06-28T13:03:43Z",
+    "order_id": 143,
+    "quantity": 2,
+    "refund_number_reference": null,
+    "state": "2",
+    "terminal_id": 2,
+    "updated_at": "2013-06-28T13:10:42Z"
+  }
+}
+]
+],
+  [
+  "2013-06-29T11:05:21+10:00",
+  [
+{
+  "order_item": {
+    "amount": "400.0",
+    "created_at": "2013-06-29T01:05:21Z",
+    "id": 261,
+    "menu_item_id": 3,
+    "name": "Belvedere",
+    "order_for": "2013-06-29T01:05:21Z",
+    "order_id": 144,
+    "quantity": 1,
+    "refund_number_reference": null,
+    "state": "0",
+    "terminal_id": null,
+    "updated_at": "2013-06-29T01:05:21Z"
+  }
+}
+]
+]
+];
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatCurrency = d3.format(".0$");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%H:%M"));
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatCurrency);
+
+var svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  x.domain(data.map(function(d) { return d3.time.format.iso.parse(d[0]); }));
+  y.domain([0, d3.max(data, function(d) { return d3.sum(d[1].map(function(item) { return item.order_item.amount; })); })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Order total");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d[0]); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d3.sum(d[1].map(function(item) { return item.order_item.amount; }))); })
+      .attr("height", function(d) { 
+        return height - y(d3.sum(d[1].map(function(item) { 
+          return item.order_item.amount; 
+        }))); })
+      .attr('fill', function(d, i) {
+        return "rgb(0, 0, " + (i * 10) + ")";
+      });
+$('svg .bar').tipsy({
+  gravity: 'w',
+  html: true,
+  title: function() {
+    var d = this.__data__;
+    order_items = d[1].map(function(item) { return item.order_item })
+    var message = "TOTAL: " + d3.sum(d[1].map(function(item) { return item.order_item.amount; })) + "<br/>";
+    order_items.forEach( function(order_item) { return message += order_item.name + ":" + order_item.amount + "$" + "<br/>"; });
+    return message;
+  }
+});
+
